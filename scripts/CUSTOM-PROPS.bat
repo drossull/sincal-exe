3476309f24fc9048e8355865256fdcc422465af7
G. Mardones <gonzonian@gmail.com>
2026-04-27 13:30:03 -0400

    Corrección de errores

--- scripts/CUSTOM-PROPS.bat
+++ scripts/CUSTOM-PROPS.bat
@@ -5,50 +5,123 @@ chcp 65001 > nul
 :: Cargar la ruta de la consola buscando en la MISMA carpeta del script
 call "%~dp0cad_env.bat"
 
+:: Verificación de seguridad
+if "%CAD_CONSOLE%"=="" (
+    echo [ERROR] No se pudo cargar la ruta de la consola.
+    pause
+    exit /b
+)
+
+:: 1. Buscar el primer DWG de la carpeta
+set "primer_dwg="
+for %%f in (*.dwg) do (
+    set "primer_dwg=%%f"
+    goto :encontrado
+)
+:encontrado
+if "%primer_dwg%"=="" (
+    echo [ERROR] No hay archivos DWG en esta carpeta.
+    pause
+    exit /b
+)
+
+echo ===================================================
+echo   ANALIZANDO PROPIEDADES ACTUALES...
+echo   Extrayendo datos de: %primer_dwg%
+echo   (Por favor espera unos segundos)
+echo ===================================================
+
+:: 2. Generar script temporal de EXTRACCION
+set "extract_scr=%~dp0TEMP_EXTRACT.scr"
+set "data_file=%~dp0TEMP_DATA.txt"
+set "data_file_lisp=%data_file:\=/%"
+
+echo (vl-load-com) > "%extract_scr%"
+echo (setq info (vla-get-SummaryInfo (vla-get-ActiveDocument (vlax-get-acad-object)))) >> "%extract_scr%"
+echo (setq f (open "%data_file_lisp%" "w")) >> "%extract_scr%"
+echo (foreach p '("Nombre_Estructura" "Provincia" "Comuna" "Revision" "Fecha_Rev" "Fecha_Inf" "No_total_planos" "Nombre_Plano") (setq val "") (vl-catch-all-apply 'vla-GetCustomByKey (list info p 'val)) (write-line (strcat p "=" val) f)) >> "%extract_scr%"
+echo (close f) >> "%extract_scr%"
+echo _.QUIT >> "%extract_scr%"
+echo. >> "%extract_scr%"
+
+:: Ejecutar extraccion silenciosa
+"%CAD_CONSOLE%" /i "%primer_dwg%" /s "%extract_scr%" > nul 2>&1
+
+:: 3. Leer los datos (CON MODO USEBACKQ PARA EVITAR ERRORES DE ESPACIO)
+set "val_Nombre_Estructura="
+set "val_Provincia="
+set "val_Comuna="
+set "val_Revision="
+set "val_Fecha_Rev="
+set "val_Fecha_Inf="
+set "val_No_total_planos="
+set "val_Nombre_Plano="
+
+if exist "%data_file%" (
+    for /f "usebackq tokens=1,* delims==" %%A in ("%data_file%") do (
+        set "val_%%A=%%B"
+    )
+    del "%data_file%"
+)
+del "%extract_scr%"
+
+:: 4. Captura interactiva
+cls
 echo ===================================================
 echo    ACTUALIZADOR MASIVO DE DWGPROPS (MULTIPLE)
 echo ===================================================
+echo Datos base leidos de: %primer_dwg%
 echo.
-echo Por favor, ingresa los datos para actualizar las viñetas.
-echo (Si dejas un campo en blanco y presionas Enter, el plano
-echo  conservará el valor que ya tiene actualmente)
+echo Presiona ENTER para mantener el valor actual [mostrado entre corchetes].
+echo Escribe un nuevo valor para sobrescribirlo.
 echo.
 
-:: Limpiar variables previas por seguridad para evitar que guarde basura en memoria
-set "nombre_est="
-set "provincia="
-set "comuna="
-set "revision="
-set "fecha_rev="
-set "fecha_inf="
-set "no_total_planos="
+set "in_nombre_est="
+set /p "in_nombre_est=1. Nombre_Estructura [%val_Nombre_Estructura%]: "
+if not defined in_nombre_est set "in_nombre_est=%val_Nombre_Estructura%"
 
-:: 1. Captura de datos comunes (Ordenados de general a particular)
-set /p nombre_est="1. Nombre_Estructura: "
-set /p provincia="2. Provincia: "
-set /p comuna="3. Comuna: "
-set /p revision="4. Revision: "
-set /p fecha_rev="5. Fecha_Rev: "
-set /p fecha_inf="6. Fecha_Inf: "
-set /p no_total_planos="7. No_total_planos: "
+set "in_provincia="
+set /p "in_provincia=2. Provincia [%val_Provincia%]: "
+if not defined in_provincia set "in_provincia=%val_Provincia%"
 
-:: 2. Ruta del script temporal (Se guarda en la misma carpeta oculta universal)
+set "in_comuna="
+set /p "in_comuna=3. Comuna [%val_Comuna%]: "
+if not defined in_comuna set "in_comuna=%val_Comuna%"
+
+set "in_revision="
+set /p "in_revision=4. Revision [%val_Revision%]: "
+if not defined in_revision set "in_revision=%val_Revision%"
+
+set "in_fecha_rev="
+set /p "in_fecha_rev=5. Fecha_Rev [%val_Fecha_Rev%]: "
+if not defined in_fecha_rev set "in_fecha_rev=%val_Fecha_Rev%"
+
+set "in_fecha_inf="
+set /p "in_fecha_inf=6. Fecha_Inf [%val_Fecha_Inf%]: "
+if not defined in_fecha_inf set "in_fecha_inf=%val_Fecha_Inf%"
+
+set "in_no_total_planos="
+set /p "in_no_total_planos=7. No_total_planos [%val_No_total_planos%]: "
+if not defined in_no_total_planos set "in_no_total_planos=%val_No_total_planos%"
+
+set "in_nombre_plano="
+set /p "in_nombre_plano=8. Nombre_Plano [%val_Nombre_Plano%]: "
+if not defined in_nombre_plano set "in_nombre_plano=%val_Nombre_Plano%"
+
+:: 5. Generacion del Script de INYECCION
 set "ruta_script=%~dp0TEMP_PROPS.scr"
-
-:: 3. Generación del Script (.scr) con condicionales
 echo (vl-load-com) > "%ruta_script%"
 echo (setq info (vla-get-SummaryInfo (vla-get-ActiveDocument (vlax-get-acad-object)))) >> "%ruta_script%"
 
-:: Inyectamos las propiedades SOLO si la variable no está vacía
-if defined nombre_est echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Nombre_Estructura" "%nombre_est%"))) (vla-AddCustomInfo info "Nombre_Estructura" "%nombre_est%")) >> "%ruta_script%"
-if defined provincia echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Provincia" "%provincia%"))) (vla-AddCustomInfo info "Provincia" "%provincia%")) >> "%ruta_script%"
-if defined comuna echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Comuna" "%comuna%"))) (vla-AddCustomInfo info "Comuna" "%comuna%")) >> "%ruta_script%"
-if defined revision echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Revision" "%revision%"))) (vla-AddCustomInfo info "Revision" "%revision%")) >> "%ruta_script%"
-if defined fecha_rev echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Rev" "%fecha_rev%"))) (vla-AddCustomInfo info "Fecha_Rev" "%fecha_rev%")) >> "%ruta_script%"
-if defined fecha_inf echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Inf" "%fecha_inf%"))) (vla-AddCustomInfo info "Fecha_Inf" "%fecha_inf%")) >> "%ruta_script%"
-if defined no_total_planos echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "No_total_planos" "%no_total_planos%"))) (vla-AddCustomInfo info "No_total_planos" "%no_total_planos%")) >> "%ruta_script%"
+if defined in_nombre_est echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Nombre_Estructura" "%in_nombre_est%"))) (vla-AddCustomInfo info "Nombre_Estructura" "%in_nombre_est%")) >> "%ruta_script%"
+if defined in_provincia echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Provincia" "%in_provincia%"))) (vla-AddCustomInfo info "Provincia" "%in_provincia%")) >> "%ruta_script%"
+if defined in_comuna echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Comuna" "%in_comuna%"))) (vla-AddCustomInfo info "Comuna" "%in_comuna%")) >> "%ruta_script%"
+if defined in_revision echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Revision" "%in_revision%"))) (vla-AddCustomInfo info "Revision" "%in_revision%")) >> "%ruta_script%"
+if defined in_fecha_rev echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Rev" "%in_fecha_rev%"))) (vla-AddCustomInfo info "Fecha_Rev" "%in_fecha_rev%")) >> "%ruta_script%"
+if defined in_fecha_inf echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Inf" "%in_fecha_inf%"))) (vla-AddCustomInfo info "Fecha_Inf" "%in_fecha_inf%")) >> "%ruta_script%"
+if defined in_no_total_planos echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "No_total_planos" "%in_no_total_planos%"))) (vla-AddCustomInfo info "No_total_planos" "%in_no_total_planos%")) >> "%ruta_script%"
+if defined in_nombre_plano echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Nombre_Plano" "%in_nombre_plano%"))) (vla-AddCustomInfo info "Nombre_Plano" "%in_nombre_plano%")) >> "%ruta_script%"
 
-:: Comandos finales de limpieza y guardado
 echo (setvar "WIPEOUTFRAME" 0) >> "%ruta_script%"
 echo _.QSAVE >> "%ruta_script%"
 echo _.QUIT >> "%ruta_script%"
@@ -57,25 +130,15 @@ echo. >> "%ruta_script%"
 echo.
 echo ---------------------------------------------------
 echo Datos capturados. Iniciando procesamiento masivo...
-echo Consola detectada: %CAD_CONSOLE%
 echo ---------------------------------------------------
 
-:: Verificación de seguridad por si no se detectó el CAD
-if "%CAD_CONSOLE%"=="" (
-    echo [ERROR] No se pudo cargar la ruta de la consola de AutoCAD/ZWCAD.
-    echo Asegurate de haber presionado "Actualizar" en el SINCAL.exe
-    del "%ruta_script%" 2>nul
-    pause
-    exit /b
-)
-
-:: 4. Ejecución Universal y Silenciosa
+:: 6. Ejecucion Universal
 for %%f in (*.dwg) do (
     echo Procesando: %%f
     "%CAD_CONSOLE%" /i "%%f" /s "%ruta_script%"
 )
 
-:: 5. Eliminación del script temporal
+:: 7. Limpieza
 del "%ruta_script%"
 
 echo.
