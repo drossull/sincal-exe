484d92beadbdea433ac8db03ef42a6782ca7ef98
G. Mardones <gonzonian@gmail.com>
2026-04-27 12:43:36 -0400

    Actualización de Custom Properties

--- scripts/CUSTOM-PROPS.bat
+++ scripts/CUSTOM-PROPS.bat
@@ -10,29 +10,43 @@ echo    ACTUALIZADOR MASIVO DE DWGPROPS (MULTIPLE)
 echo ===================================================
 echo.
 echo Por favor, ingresa los datos para actualizar las viñetas.
-echo (Se omitió "Nombre_Plano" para preservar la identidad de cada lámina)
+echo (Si dejas un campo en blanco y presionas Enter, el plano
+echo  conservará el valor que ya tiene actualmente)
 echo.
 
-:: 1. Captura de datos comunes
+:: Limpiar variables previas por seguridad para evitar que guarde basura en memoria
+set "nombre_est="
+set "provincia="
+set "comuna="
+set "revision="
+set "fecha_rev="
+set "fecha_inf="
+set "no_total_planos="
+
+:: 1. Captura de datos comunes (Ordenados de general a particular)
 set /p nombre_est="1. Nombre_Estructura: "
-set /p revision="2. Revision: "
-set /p fecha_rev="3. Fecha_Rev: "
-set /p fecha_inf="4. Fecha_Inf: "
-set /p no_total_planos="5. No_total_planos: "
+set /p provincia="2. Provincia: "
+set /p comuna="3. Comuna: "
+set /p revision="4. Revision: "
+set /p fecha_rev="5. Fecha_Rev: "
+set /p fecha_inf="6. Fecha_Inf: "
+set /p no_total_planos="7. No_total_planos: "
 
 :: 2. Ruta del script temporal (Se guarda en la misma carpeta oculta universal)
 set "ruta_script=%~dp0TEMP_PROPS.scr"
 
-:: 3. Generación del Script (.scr) con las 5 propiedades restantes
+:: 3. Generación del Script (.scr) con condicionales
 echo (vl-load-com) > "%ruta_script%"
 echo (setq info (vla-get-SummaryInfo (vla-get-ActiveDocument (vlax-get-acad-object)))) >> "%ruta_script%"
 
-:: Inyectamos las propiedades
-echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Nombre_Estructura" "%nombre_est%"))) (vla-AddCustomInfo info "Nombre_Estructura" "%nombre_est%")) >> "%ruta_script%"
-echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Revision" "%revision%"))) (vla-AddCustomInfo info "Revision" "%revision%")) >> "%ruta_script%"
-echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Rev" "%fecha_rev%"))) (vla-AddCustomInfo info "Fecha_Rev" "%fecha_rev%")) >> "%ruta_script%"
-echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Inf" "%fecha_inf%"))) (vla-AddCustomInfo info "Fecha_Inf" "%fecha_inf%")) >> "%ruta_script%"
-echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "No_total_planos" "%no_total_planos%"))) (vla-AddCustomInfo info "No_total_planos" "%no_total_planos%")) >> "%ruta_script%"
+:: Inyectamos las propiedades SOLO si la variable no está vacía
+if defined nombre_est echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Nombre_Estructura" "%nombre_est%"))) (vla-AddCustomInfo info "Nombre_Estructura" "%nombre_est%")) >> "%ruta_script%"
+if defined provincia echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Provincia" "%provincia%"))) (vla-AddCustomInfo info "Provincia" "%provincia%")) >> "%ruta_script%"
+if defined comuna echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Comuna" "%comuna%"))) (vla-AddCustomInfo info "Comuna" "%comuna%")) >> "%ruta_script%"
+if defined revision echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Revision" "%revision%"))) (vla-AddCustomInfo info "Revision" "%revision%")) >> "%ruta_script%"
+if defined fecha_rev echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Rev" "%fecha_rev%"))) (vla-AddCustomInfo info "Fecha_Rev" "%fecha_rev%")) >> "%ruta_script%"
+if defined fecha_inf echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "Fecha_Inf" "%fecha_inf%"))) (vla-AddCustomInfo info "Fecha_Inf" "%fecha_inf%")) >> "%ruta_script%"
+if defined no_total_planos echo (if (vl-catch-all-error-p (vl-catch-all-apply 'vla-SetCustomByKey (list info "No_total_planos" "%no_total_planos%"))) (vla-AddCustomInfo info "No_total_planos" "%no_total_planos%")) >> "%ruta_script%"
 
 :: Comandos finales de limpieza y guardado
 echo (setvar "WIPEOUTFRAME" 0) >> "%ruta_script%"
--- startup/AutoCrearPropiedad.lsp
+++ startup/AutoCrearPropiedad.lsp
@@ -7,36 +7,37 @@
   (setq props (vlax-get-property doc 'SummaryInfo))
 
   ;; --- CONFIGURACION ---
-  ;; Lista de propiedades a crear (formato: "Nombre" . "Valor por defecto")
+  ;; Lista de propiedades organizada de lo general a lo particular
   (setq listaProps
     '(
-      ("Nombre_Plano"      . "Ingrese nombre plano")
       ("Nombre_Estructura" . "Ingrese nombre estructura")
-      ("Revision"	   . "rev")
-      ("Fecha_Rev"	   . "F_REV")
-      ("Fecha_Inf"	   . "F_INF")
+      ("Provincia"         . "Ingrese provincia")
+      ("Comuna"            . "Ingrese comuna")
+      ("Revision"          . "rev")
+      ("Fecha_Rev"         . "F_REV")
+      ("Fecha_Inf"         . "F_INF")
       ("No_total_planos"   . "Ingrese número total de planos")
+      ("Nombre_Plano"      . "Ingrese nombre plano")
      )
   )
   ;; ---------------------
 
   ;; ESTRATEGIA: Recorrer la lista e intentar agregar cada propiedad
   (foreach prop listaProps
-    ;; El metodo interno en ActiveX se llama "AddCustomInfo"
     (setq err 
       (vl-catch-all-apply 
         'vlax-invoke-method
-        ;; (car prop) extrae el nombre, (cdr prop) extrae el valor
         (list props 'AddCustomInfo (car prop) (cdr prop))
       )
     )
 
-    ;; Si no hubo error, significa que se creo. 
-    ;; Si hubo error, probablemente ya existia, asi que pasamos a la siguiente.
+    ;; Si se crea con éxito, informa al usuario.
+    ;; Si falla (porque ya existe), el programa continúa sin detenerse.
     (if (not (vl-catch-all-error-p err))
        (princ (strcat "\n[AutoCAD] Propiedad '" (car prop) "' creada exitosamente."))
     )
   )
+  (princ "\n[AutoCAD] Proceso de creación de propiedades finalizado.")
   (princ)
 )
 
